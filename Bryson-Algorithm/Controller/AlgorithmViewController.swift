cc2f90616b420290db90d293f64c7aa658ec517e
Bryson Spaulding <brysonian10@hotmail.com>
2018-10-23 08:40:48 -0600

    Connected some nodes to the code and now starting to program the steps for the algorithm.

---
+++ Bryson-Algorithm/Controller/AlgorithmViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AlgorithmViewController.swift
+//  Bryson-Algorithm
+//
+//  Created by Spaulding, Bryson on 10/23/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class AlgorithmViewController: UIViewController
+{
+
+    @IBOutlet weak var algorithmText: UILabel!
+    
+    @IBOutlet weak var swiftImage: UIImageView!
+    
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        formatAlgorithm()
+    }
+    
+    private func formatAlgorithm() -> Void
+    {
+        let stepOne : String = ""
+        let stepTwo : String = ""
+        let stepThree : String = ""
+    
+    }
+    
+}
+
